91c491bf2cad405167c63d61ba75521fffcd332f
Daniel Wilkins <daniel.wilkins@centeva.com>
2019-01-08 16:53:16 -0700

    first steps to getting it working

--- README.md
+++ README.md
@@ -27,3 +27,13 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+# steps I took
+1. npm install -g @vue/cli
+2. rustup target add wasm32-unknown-unknown --toolchain nightly
+3. cargo +nightly install wasm-bindgen-cli
+
+
+4. cargo +nightly build --target wasm32-unknown-unknown
+5. wasm-bindgen target/wasm32-unknown-unknown/debug/csv2sql.wasm --out-dir .\src\rust\
+4. npm run serve to build and server the vue.js app
--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button v-on:click="bacon('Sucks')">Say hello from rust</button>
+    <HelloWorld msg="Hello World"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+//const rust = import('./rust/csv2sql')
+var rust = import('./rust/csv2sql')
 
 export default {
   name: 'app',
   components: {
     HelloWorld
+  },
+  methods: {
+    bacon: (f) => { 
+      // eslint-disable-next-line
+      console.log(rust);  
+      rust._v.greet(f);
+    }
   }
 }
 </script>
