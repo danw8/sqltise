71e8e1e52c4ca9417b94397566a46d581ef6a845
Daniel Wilkins <daniel.wilkins@wilxoft.com>
2019-01-09 06:46:42 -0700

    styled the files selection input.

--- ui/src/components/LoadCsvFile.vue
+++ ui/src/components/LoadCsvFile.vue
@@ -1,7 +1,10 @@
 <template>
 	<form id="fileform" ref="fileform">
 		<div>Load the csv file to use as the source</div>
-		<input type="file" id="files" name="csvfile" accept="text/csv" v-on:change="fileSelected"/>
+		<div class="file-upload">
+			<input type="file" id="files" ref="files" name="csvfile" class="csv-upload" accept="text/csv" v-on:change="fileSelected"/>
+			<div class="csv-upload-label"  v-on:click="selectfile"><i class="fa fa-file-upload upload-icon"></i><span ref="filelabel">Choose a file</span></div>
+		</div>
 	</form>
 </template>
 
@@ -33,6 +36,8 @@ export default {
 				return;
 			}
 
+			this.$refs.filelabel.textContent = file.name;
+
 			var reader = new FileReader();
 			reader.readAsText(file);
 			reader.onload = () => {
@@ -40,6 +45,9 @@ export default {
 				// this seems to work except I'm not listening on the parent yet
 				this.$root.$emit('loaded', file);
 			}
+		},
+		selectfile: function () {
+			this.$refs.files.click();
 		}
 	}
 }
@@ -51,5 +59,46 @@ export default {
 	width: 200px;
 	height: 50px;
 	background-color: black;
+}
+
+.file-upload {
+	position: relative;
+}
+
+.csv-upload-label {
+	position: absolute;
+	top: 0px;
+	left: 0px;
+	width: 240px;
+	height: 60px;
+	z-index: 2;
+	color: white;
+	font-size: 1.25em;
+	background-color: #277554;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	cursor: pointer;
+}
+
+.csv-upload-label:hover {
+	background-color: #499273;
+}
+
+.csv-upload {
+	position: relative;
+	text-align: right;
+	-moz-opacity: 0;
+	filter:alpha(opacity:0);
+	opacity: 0;
+	z-index: 1;
+	width: 240px;
+	height: 60px;
+	cursor: pointer;
+}
+
+.upload-icon{
+	font-size: 30px;
+	padding-right: 10px;
 }
 </style>
