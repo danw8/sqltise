dac82bba1877a2e4b0385fcf28376350d35ea14f
Daniel Wilkins <daniel.wilkins@centeva.com>
2020-02-26 15:41:31 -0700

    cargo fmt

--- sqltise_cli/src/main.rs
+++ sqltise_cli/src/main.rs
@@ -27,38 +27,44 @@ fn main() {
 		.subcommand(
 			SubCommand::with_name("add")
 				.about("adds selections to the project")
-                .arg(Arg::with_name("selection")
-                     .help("the type to add")
-                     .possible_values(&["statement", "sourcefile", "project"])
-                     .index(1)
-                     .required(true))
+				.arg(
+					Arg::with_name("selection")
+						.help("the type to add")
+						.possible_values(&["statement", "sourcefile", "project"])
+						.index(1)
+						.required(true),
+				),
 		)
 		.subcommand(
 			SubCommand::with_name("remove")
 				.about("remove selections from the project")
-                .arg(Arg::with_name("selection")
-                     .help("the type to remove")
-                     .possible_values(&["statement", "sourcefile", "project"])
-                     .index(1)
-                     .required(true))
+				.arg(
+					Arg::with_name("selection")
+						.help("the type to remove")
+						.possible_values(&["statement", "sourcefile", "project"])
+						.index(1)
+						.required(true),
+				),
 		)
-        .subcommand(
-            SubCommand::with_name("list")
-                .about("list the current selections from the project")
-                .arg(Arg::with_name("selection")
-                     .help("the type to list")
-                     .possible_values(&["statement", "sourcefile", "all"])
-                     .index(1)
-                     .required(true))
-        )
+		.subcommand(
+			SubCommand::with_name("list")
+				.about("list the current selections from the project")
+				.arg(
+					Arg::with_name("selection")
+						.help("the type to list")
+						.possible_values(&["statement", "sourcefile", "all"])
+						.index(1)
+						.required(true),
+				),
+		)
 		.get_matches();
 
 	let project_name = matches.value_of("project").unwrap_or("test");
 	dbg!(project_name);
 
 	if let Some(matches) = matches.subcommand_matches("add") {
-        let selection = value_t!(matches, "selection",  Selections).expect("Must supply a variant");
-        let x = format!("add {}", selection);
+		let selection = value_t!(matches, "selection", Selections).expect("Must supply a variant");
+		let x = format!("add {}", selection);
 		dbg!(x);
 	}
 	if let Some(matches) = matches.subcommand_matches("remove") {
--- sqltise_web/src/lib.rs
+++ sqltise_web/src/lib.rs
@@ -3,31 +3,29 @@ use wasm_bindgen::prelude::*;
 // lifted from the `console_log` example
 #[wasm_bindgen]
 extern "C" {
-    #[wasm_bindgen(js_namespace = console)]
-    fn log(a: &str);
+	#[wasm_bindgen(js_namespace = console)]
+	fn log(a: &str);
 }
 
 macro_rules! console_log {
     ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
 }
 
-
 // Called when the wasm module is instantiated
 #[wasm_bindgen(start)]
 pub fn main() -> Result<(), JsValue> {
 	console_log!("running main.");
-    // Use `web_sys`'s global `window` function to get a handle on the global
-    // window object.
-    let window = web_sys::window().expect("no global `window` exists");
-    let document = window.document().expect("should have a document on window");
-    let body = document.body().expect("document should have a body");
+	// Use `web_sys`'s global `window` function to get a handle on the global
+	// window object.
+	let window = web_sys::window().expect("no global `window` exists");
+	let document = window.document().expect("should have a document on window");
+	let body = document.body().expect("document should have a body");
 
-    // Manufacture the element we're gonna append
-    let val = document.create_element("p")?;
-    val.set_inner_html("Hello from Rust!");
+	// Manufacture the element we're gonna append
+	let val = document.create_element("p")?;
+	val.set_inner_html("Hello from Rust!");
 
-    body.append_child(&val)?;
+	body.append_child(&val)?;
 
-    Ok(())
+	Ok(())
 }
-
